f15ec187f748e07be1cca0c2abdfbc522f450c50
Viktor Petrov <viktor_petrov@epam.com>
2024-02-03 18:22:17 +0100

    init

---
+++ src/test/java/spring/security/fundamentals/SpringSecurityFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFundamentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
