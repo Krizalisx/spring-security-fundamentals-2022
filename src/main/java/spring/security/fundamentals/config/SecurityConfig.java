fa012fa50fecd2e37b69ccf33466c3327b9450c1
Viktor Petrov <viktor_petrov@epam.com>
2024-02-14 20:24:47 +0100

    Lesson 5. Part 1. Learned different rules for request matchers.

--- src/main/java/spring/security/fundamentals/config/SecurityConfig.java
+++ src/main/java/spring/security/fundamentals/config/SecurityConfig.java
@@ -1,26 +1,57 @@
 package spring.security.fundamentals.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import spring.security.fundamentals.security.filters.ApiKeyAuthenticationFilter;
 
 @Configuration
-@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final ApiKeyAuthenticationFilter apiKeyAuthenticationFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security, UserDetailsService userDetailsService) throws Exception {
+        return security.httpBasic(Customizer.withDefaults())
+            .authorizeHttpRequests(customizer -> customizer
+//                .anyRequest().permitAll()
+//                .anyRequest().denyAll()
+//                .anyRequest().hasAuthority("read")
+//                .anyRequest().hasAnyAuthority("read", "write")
+//                .anyRequest().hasRole("USER")
+//                .anyRequest().hasAnyRole("USER", "ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .build();
+    }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.httpBasic(Customizer.withDefaults())
-            .addFilterBefore(apiKeyAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails user1 = User.withUsername("krizalis")
+            .password(passwordEncoder().encode("password"))
+            .authorities("read")
             .build();
+
+        UserDetails user2 = User.withUsername("krizalis2")
+            .password(passwordEncoder().encode("password2"))
+            .authorities("write")
+            .build();
+
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(user2);
+
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/spring/security/fundamentals/controller/DemoController.java
+++ src/main/java/spring/security/fundamentals/controller/DemoController.java
@@ -1,7 +1,5 @@
 package spring.security.fundamentals.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class DemoController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(authentication);
-        return "hello";
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "Demo!";
     }
 
 }
